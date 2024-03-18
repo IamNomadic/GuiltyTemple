98ab84d79ea664a6a9957621ed8540f32463970d
IamNomadic <111480835+IamNomadic@users.noreply.github.com>
2024-03-17 18:53:51 -0700

    Add files via upload

---
+++ Scripts/Player/PlayerHumanState.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class PlayerHumanState : PlayerBaseState
+{
+    
+
+    public override void EnterState(PlayerStateManager player)
+    {
+        Debug.Log("StateTest");
+    }
+    public override void UpdateState(PlayerStateManager player)
+    {
+       
+    }
+    public override void ExitState(PlayerStateManager player)
+    {
+        
+    }
+    public override void FixedUpdateState(PlayerStateManager player)
+    {
+       
+    }
+
+
+
+
+
+
+
+}
+
