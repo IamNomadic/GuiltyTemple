363bbfdeec5020952ee187a775e3935747d5eedc
Damien <damienjianyi@gmail.com>
2024-04-03 20:38:23 -0600

    Finished Title Screen (mostly)
    finished title screen aside from options menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -7,17 +7,7 @@ public class PauseMenu : MonoBehaviour
     public static bool GameIsPaused = false;
     public GameObject PauseUi;
         
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
     public void Pause()
     {
         if (GameIsPaused)
