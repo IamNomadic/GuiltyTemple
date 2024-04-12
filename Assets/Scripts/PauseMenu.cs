127628a1f25349bda74c3928399abf4c266b58b6
Damien <damienjianyi@gmail.com>
2024-04-12 15:03:40 -0600

    more ui stuff

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
     public GameObject PauseUi;
-        
-    
+    public AudioSource Source;
+    public AudioClip ButtonHover;
+    public AudioClip ButtonPressed;
+
     public void Pause()
     {
         if (GameIsPaused)
@@ -30,6 +33,36 @@ public class PauseMenu : MonoBehaviour
         PauseUi.SetActive(true);
         Time.timeScale = 0;
         GameIsPaused = true;
+    }
+    public void OnHover()
+    {
+        Source.PlayOneShot(ButtonHover);
+    }
+    public void OnPlayButton()
+    {
+        Source.PlayOneShot(ButtonPressed);
+        ResumeGame();
+    }
+    public void OnRestartButton()
+    {
+        Source.PlayOneShot(ButtonPressed);
+        StartCoroutine(RestartGame());
+    }
+    public void OnExitButton()
+    {
+        Source.PlayOneShot(ButtonPressed);
+        StartCoroutine(ExitGame());
+    }
+    IEnumerator ExitGame()
+    {
+        yield return new WaitForSeconds(1f);
+        Debug.Log("Exited Game");
+        Application.Quit();
+    }
+    IEnumerator RestartGame()
+    {
+        yield return new WaitForSeconds(.5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
 
